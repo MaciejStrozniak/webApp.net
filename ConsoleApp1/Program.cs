5d9f280e3f1b35aab88866fa4b87aa3048041f62
msCDV <macgitcdv@gmail.com>
2025-02-03 12:16:44 +0100

    Added some basic code for testing purpose.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int numberNew = 10;
+string wordNew = "Ciasteczko";
+
+void FunctionNew(int number, string word) {
+    Console.WriteLine("Słowo to " + word + " powtórzone " + number + " razy.");
+};
+
+int ChangeNumberNew(int number, int numberToAdd) {
+
+    int changedNumberNew = 0;
+    return changedNumberNew = number + numberToAdd;
+
+}
+
+// --------------------------------------------------------------- //
+
+Console.WriteLine("Hello, World! 2 ");
+FunctionNew(ChangeNumberNew(numberNew, 56), wordNew);
+// Console.WriteLine(FunctionNew());
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0b3666768e800641e10989010adf9460e00de0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203e3c43522fc47e3bd1b2401cde72facd113da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
